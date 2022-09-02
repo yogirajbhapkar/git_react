9c875d8c0f9075ca5981ace1847781f20976b736
yogirajbhapkar <yogirajworks@gmail.com.commit>
2022-09-02 18:23:57 +0530

    first commit

---
+++ src/components/First_compo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function First_compo(){
+    return (
+        <div>
+            <h1>    First Componenet </h1>
+
+        </div>
+    )
+}
+
+export default First_compo
